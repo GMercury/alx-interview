549c7f698d433fdad9b0fb64a73ea2c1ba053d21
GMercury <99487197+GMercury@users.noreply.github.com>
2023-01-22 12:10:54 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,46 @@
 #!/usr/bin/python3
 
+""" script that reads stdin line by line and computes metrics """
+
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        if len(parsed_line) > 2:
-            counter += 1
+        stlist = line.split()
+        count += 1
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
 
-finally:
-    print_msg(dict_sc, total_file_size)
+except KeyboardInterrupt:
+    printStatus(statusCodes, size)
+    raise
